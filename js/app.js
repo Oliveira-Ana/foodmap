793cfff8c77444ea3b341da237e70abd1144ff6e
Ana Oliveira <ana_oli@outlook.com>
2018-08-01 16:54:05 -0300

    Iniciando os trabalhos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // Add your JavaScript
+ $.each( restaurantes, function( index , value ) {
+    console.log( 'valor:'+ value.image ); 
+});
+ 
+// console.log( sum ); 
+
+// $.each( , function() {
+//     console.log(value);
+//   });
