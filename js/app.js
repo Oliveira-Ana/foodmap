95c123c5083d220bbad0a18451c4ea9363ac6cc2
Ana Oliveira <ana_oli@outlook.com>
2018-08-02 21:00:32 -0300

    test de filtro

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 // Add your JavaScript
- $.each( restaurantes, function( index , value ) {
-    console.log( 'valor:'+ value.image ); 
-});
- 
+ //$.each( restaurantes, function( index , value ) {
+ //   console.log( 'valor:'+ value.image ); 
+//});
 // console.log( sum ); 
 
 // $.each( , function() {
 //     console.log(value);
 //   });
+
+//$(document).ready(function() {
+//var  userChoice;
+
+//$(document).keypress(function getValue(e){
+  //  if(e.which === 13){
+    //    userChoice = $('input').val();
+      //  getRestaurantImage();
+    //}
+//})
+
+//function getRestaurantImage(){
+  //  $.each(restaurantes, function (index,value){
+    //    if (userChoice === value.name) {
+      //      $('div').html('<img src="assets/' +  value.image + '"></img>');
+       // }
+   // })
+//};
+  
+//})
+$(document).ready(function() {
+    $(document).ready(function() {
+        $('.box-2').delay('5000').fadeIn('slow')
+      });
+    })
